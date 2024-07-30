64beb9fe3bff9a6cd34ccd49c2cdf278437e4f56
Victor Hugo <abjurandam@gmail.com>
2024-07-30 18:48:20 -0300

    Add files via upload

---
+++ src/main/java/com/github/wilyJ80/hello/HelloController.java
@@ -0,0 +1,13 @@
+package com.github.wilyJ80.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String sayHi() {
+		return "Welcome";
+	}
+}
