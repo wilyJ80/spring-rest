8bbd8649719c8cc1463d8bffe77e5bc4cd520afa
wilyJ80 <abjurandam@gmail.com>
2024-07-30 22:18:41 -0300

    Implement POST

--- src/main/java/com/github/wilyJ80/topic/TopicService.java
+++ src/main/java/com/github/wilyJ80/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.github.wilyJ80.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,10 +8,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("spring", "Spring Framework", "Spring Framework Description"),
 			new Topic("java", "Core Java", "Core Java Description"),
-			new Topic("javascript", "JavaScript", "JavaScript Description"));
+			new Topic("javascript", "JavaScript", "JavaScript Description")));
 
 	public List<Topic> getAllTopics() {
 		return topics;
@@ -18,6 +19,10 @@ public class TopicService {
 
 	public Topic getTopic(String id) {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 
 }
