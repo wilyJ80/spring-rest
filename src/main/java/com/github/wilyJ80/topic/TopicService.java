8a310964cd605c3bd87e3e632630a41bef9941fc
wilyJ80 <abjurandam@gmail.com>
2024-08-01 13:42:41 -0300

    Stream GET, TODO: use HashMap for simplified retrieval

--- src/main/java/com/github/wilyJ80/topic/TopicService.java
+++ src/main/java/com/github/wilyJ80/topic/TopicService.java
@@ -18,7 +18,10 @@ public class TopicService {
 	}
 
 	public Topic getTopic(String id) {
-		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return topics.stream()
+				.filter(t -> t.getId().equals(id))
+				.findFirst()
+				.get();
 	}
 
 	public void addTopic(Topic topic) {
