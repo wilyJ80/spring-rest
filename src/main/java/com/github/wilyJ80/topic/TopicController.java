dd13a6f04040d3020678c38a450df3d030675e28
wilyJ80 <abjurandam@gmail.com>
2024-08-09 18:13:11 -0300

    Mock tests

--- src/main/java/com/github/wilyJ80/topic/TopicController.java
+++ src/main/java/com/github/wilyJ80/topic/TopicController.java
@@ -3,12 +3,14 @@ package com.github.wilyJ80.topic;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,13 +30,16 @@ public class TopicController {
 	}
 
 	@PostMapping("/topics")
-	public void addTopic(@RequestBody Topic topic) {
+	@ResponseStatus(HttpStatus.CREATED)
+	public Topic addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
+		return topic;
 	}
 
 	@PutMapping("/topics/{id}")
-	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+	public Topic updateTopic(@RequestBody Topic topic, @PathVariable String id) {
 		topicService.updateTopic(id, topic);
+		return topic;
 	}
 
 	@DeleteMapping("/topics/{id}")
--- src/test/java/com/github/wilyJ80/CourseApiDataApplicationTests.java
+++ src/test/java/com/github/wilyJ80/CourseApiDataApplicationTests.java
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -23,10 +27,46 @@ class CourseApiDataApplicationTests {
 	}
 
 	@Test
-	void shouldReturnDefaultMessage() throws Exception {
+	void shouldTestTopicUsage() throws Exception {
+
 		this.mockMvc.perform(get("/topics")).andDo(print())
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$").isArray())
 				.andExpect(jsonPath("$", hasSize(0)));
+
+		String javascriptJSON = "{ \"id\": \"javascript\", \"name\": \"JavaScript\", \"description\": \"Best Language Ever\"}";
+
+		this.mockMvc.perform(post("/topics").contentType(MediaType.APPLICATION_JSON).content(javascriptJSON))
+				.andExpect(status().isCreated()).andExpect(jsonPath("$.id").value("javascript"))
+				.andExpect(jsonPath("$.name").value("JavaScript"))
+				.andExpect(jsonPath("$.description").value("Best Language Ever"));
+
+		String javaJSON = "{ \"id\": \"java\", \"name\": \"Java\", \"description\": \"Second Best Language Ever\"}";
+
+		this.mockMvc.perform(post("/topics").contentType(MediaType.APPLICATION_JSON).content(javaJSON))
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.id").value("java"))
+				.andExpect(jsonPath("$.name").value("Java"))
+				.andExpect(jsonPath("$.description").value("Second Best Language Ever"));
+
+		this.mockMvc.perform(get("/topics")).andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$").isArray())
+				.andExpect(jsonPath("$", hasSize(2)));
+
+		this.mockMvc.perform(delete("/topics/java")).andDo(print())
+				.andExpect(status().isOk());
+
+		this.mockMvc.perform(get("/topics")).andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$").isArray())
+				.andExpect(jsonPath("$", hasSize(1)));
+
+		String typescriptJSON = "{ \"id\": \"typescript\", \"name\": \"TypeScript\", \"description\": \"Fake typing for the nerds\"}";
+
+		this.mockMvc.perform(put("/topics/javascript").contentType(MediaType.APPLICATION_JSON).content(typescriptJSON))
+				.andExpect(status().isOk()).andExpect(jsonPath("$.id").value("typescript"))
+				.andExpect(jsonPath("$.name").value("TypeScript"))
+				.andExpect(jsonPath("$.description").value("Fake typing for the nerds"));
 	}
 }
