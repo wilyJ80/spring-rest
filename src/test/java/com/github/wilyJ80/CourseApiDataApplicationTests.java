3c3ee854a84f30e074e5cec18b9025882aaf5e11
wilyJ80 <abjurandam@gmail.com>
2024-08-09 17:14:03 -0300

    Test initial JSON

--- src/test/java/com/github/wilyJ80/CourseApiDataApplicationTests.java
+++ src/test/java/com/github/wilyJ80/CourseApiDataApplicationTests.java
@@ -1,13 +1,31 @@
 package com.github.wilyJ80;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class CourseApiDataApplicationTests {
+
+	@Autowired
+	MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/topics")).andDo(MockMvcResultHandlers.print())
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$").isArray())
+				.andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(0)));
+	}
 }
